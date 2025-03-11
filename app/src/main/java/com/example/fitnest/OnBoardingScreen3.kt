cf0c33e635d105aaf0b99517cbb0da35ebb6d29d
Romesh Gamadikari <gamadikarir@gmail.com>
2025-03-11 23:06:33 +0530

    OnBoardingScreens  commit

--- app/src/main/java/com/example/fitnest/OnBoardingScreen3.kt
+++ app/src/main/java/com/example/fitnest/OnBoardingScreen3.kt
@@ -8,27 +8,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class OnBoardingScreen2 : AppCompatActivity() {
+class OnBoardingScreen3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_on_boarding_screen2)
+        setContentView(R.layout.activity_on_boarding_screen3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        setContentView(R.layout.activity_on_boarding_screen2)
-
-        val nextbtn2 = findViewById<Button>(R.id.nextbtn2)
-
-        nextbtn2.setOnClickListener{
-            intent = Intent(this,OnBoardingScreen3::class.java)
-            startActivity(intent)
-
-        }
-
 
     }
 }
