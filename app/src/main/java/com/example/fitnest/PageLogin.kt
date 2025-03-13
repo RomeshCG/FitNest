665a401cbcb1932ce2bab40ce03712c9e843ae13
Romesh Gamadikari <gamadikarir@gmail.com>
2025-03-13 11:04:46 +0530

    OnBoardingScreens  commit

---
+++ app/src/main/java/com/example/fitnest/PageLogin.kt
@@ -0,0 +1,57 @@
+package com.example.fitnest
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PageLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_page_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Forgot Password Navigation
+        val forgotPasswordText = findViewById<TextView>(R.id.forgotPasswordText)
+        forgotPasswordText.setOnClickListener {
+            val intent = Intent(this,FrogetPassword::class.java)
+            startActivity(intent)
+        }
+
+        // Login Button
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            intent =Intent(this,Home::class.java)
+            startActivity(intent)
+
+        }
+
+        // Google Login Button
+        val googleLoginButton = findViewById<Button>(R.id.googleLoginButton)
+        googleLoginButton.setOnClickListener {
+            // Handle Google Sign-In logic
+        }
+
+        // Facebook Login Button
+        val facebookLoginButton = findViewById<Button>(R.id.facebookLoginButton)
+        facebookLoginButton.setOnClickListener {
+            // Handle Facebook Sign-In logic
+        }
+
+        // Register Text Navigation
+        val registerText = findViewById<TextView>(R.id.registerText)
+        registerText.setOnClickListener {
+            val intent = Intent(this,Register::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/fitnest/Register.kt
+++ app/src/main/java/com/example/fitnest/Register.kt
@@ -1,29 +1,20 @@
 package com.example.fitnest
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class OnBoardingScreen3 : AppCompatActivity() {
+class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_on_boarding_screen3)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val nextbtn3 = findViewById<Button>(R.id.nextbtn3)
-        nextbtn3.setOnClickListener{
-            val intent = Intent(this,PageLogin::class.java)
-            startActivity(intent)
-        }
-
     }
 }
