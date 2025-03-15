86938297df2c1d6a51442ac46b0e1febaae548c7
Romesh Gamadikari <gamadikarir@gmail.com>
2025-03-15 15:35:25 +0530

    Basic All Done  commit

--- app/src/main/java/com/example/fitnest/MealPlan.kt
+++ app/src/main/java/com/example/fitnest/MealPlan.kt
@@ -1,11 +1,13 @@
 package com.example.fitnest
 
-import androidx.fragment.app.viewModels
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.viewModels
 
 class MealPlan : Fragment() {
 
@@ -17,14 +19,26 @@ class MealPlan : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // TODO: Use the ViewModel
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_meal_plan, container, false)
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_meal_plan, container, false)
+
+        // Find the ImageView
+        val mealPlan1Image = view.findViewById<ImageView>(R.id.mealPlan1Image)
+
+        // Set OnClickListener
+        mealPlan1Image.setOnClickListener {
+            // Redirect to MealPlanActivity
+            val intent = Intent(activity, MealPlanSample::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
